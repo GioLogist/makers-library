c814311e0307aff76544e43e7a46235fac25d753
GioLogist <gio@giologist.com>
2020-09-23 17:04:18 -0700

    FeathersJS: utilize shared-lib

--- api/src/index.js
+++ api/src/index.js
@@ -3,11 +3,13 @@ const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
+const { logHello } = require('shared-lib');
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
+server.on('listening', () =>{
+  logHello()
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+});
